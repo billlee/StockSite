95ea459cc564dbb44627cfa49f11ce2002cc8f0e
Kendric <ken_dric@hotmail.com>
2019-05-03 17:31:24 -0400

    branching code

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, jsonify, abort, make_response, g
 from flask import current_app, request, render_template, redirect, url_for
 import sqlite3, requests, json, datetime
+import random as rand
 
 bp = Blueprint('main',__name__)
+
+
 
 @bp.route('/StockApp/')
 def home():
@@ -10,25 +13,57 @@ def home():
     return render_template('main.html', company_tickers=tickers)
 
 
-@bp.route('/StockApp/fetchStockPrice', methods=['GET'])
+@bp.route('/StockApp/fetchStockPrice', methods=['GET', 'POST'])
 def priceFetchAPI():
+    companies_map = {"AMZN" : "Amazon", "GOOG" : "Google", "ATVI" : "Activision", "NFLX" : "Netflix", "HD" : "Home Depot", "KR" : "Kroger", "AAPL" : "Apple", "FB" : "Facebook", "TSLA" : "Tesla", "DIS" : "Disney"}
     company_tickers = get_all_tickers()
-    now = datetime.datetime.now()
-    tickers = request.args.get('tickers')
-    if tickers is not None:
-        tickers = tickers.split("-")
 
-    queryType = request.args.get('queryType')
-    if queryType is None:
-        queryType = "history"
-    
-    startDate = request.args.get('startDate')
-    endDate = request.args.get('endDate')
+    if request.method == 'GET':
+        tickers = request.args.get('tickers')
+        if tickers is not None:
+            tickers = tickers.split("-")
 
-    data = dict_historical_data(tickers, queryType, startDate, endDate)
-    print(data)
+        queryType = request.args.get('queryType')
+        if queryType is None:
+            queryType = "history"
+        startDate = request.args.get('startDate')
+        endDate = request.args.get('endDate')
 
-    return render_template('table.html', data=data, company_tickers=company_tickers, queryType= queryType)
+
+        if queryType == "realtime":
+            print("REAL TIME GET")
+
+            data = dict_real_time_data(tickers)
+            return render_template('table.html', data=data, tickers = tickers, company_tickers=company_tickers, queryType= queryType, company_map = companies_map)
+
+        
+        startDate = request.args.get('startDate')
+        endDate = request.args.get('endDate')
+
+        data = dict_historical_data(tickers, queryType, startDate, endDate)
+        return render_template('table.html', data=data, tickers = tickers, company_tickers=company_tickers, queryType= queryType, company_map = companies_map)
+
+    if request.method == 'POST':
+        tickers = request.form.get('box.0')
+        if tickers is not None:
+            tickers = tickers.split("-")
+            if len(tickers) == 1:
+                tickers = tickers[0]
+            else:
+                tickers = tickers[0:len(tickers) - 1]
+
+
+        queryType = request.form.get('param.0')
+
+        if queryType == "realtime":
+            data = dict_real_time_data(tickers)
+            return render_template('table.html', data=data, tickers = tickers, company_tickers=company_tickers, queryType= queryType, company_map = companies_map)
+
+
+        startDate = request.form.get('start.0')
+        endDate = request.form.get('end.0')
+        data = dict_historical_data(tickers, queryType, startDate, endDate)
+        return render_template('table.html', data=data, tickers = tickers, company_tickers=company_tickers, queryType= queryType, company_map = companies_map)
 
 
 
@@ -95,13 +130,13 @@ def dict_historical_data(tickers, queryType, startDate, endDate):
                     command = "SELECT * FROM quotes WHERE ticker = ? "
                     
             elif queryType == "average":
-                    command = "SELECT ticker, date-time, AVG(open), AVG(high), AVG(low), AVG(close), AVG(volume) FROM quotes WHERE ticker = ? "
+                    command = "SELECT quotes_id, ticker, date_time, AVG(open), AVG(high), AVG(low), AVG(close), AVG(volume) FROM quotes WHERE ticker = ? "
                     
             elif queryType == "low":
-                    command = "SELECT ticker, date-time, MIN(open), MIN(high), MIN(low), MIN(close), MIN(volume) FROM quotes WHERE ticker = ? "
+                    command = "SELECT quotes_id, ticker, date_time, MIN(open), MIN(high), MIN(low), MIN(close), MIN(volume) FROM quotes WHERE ticker = ? "
                     
             elif queryType == "high":
-                    command = "SELECT ticker, date-time, MAX(open), MAX(high), MAX(low), MAX(close), MAX(volume) FROM quotes WHERE ticker = ? "
+                    command = "SELECT quotes_id, ticker, date_time, MAX(open), MAX(high), MAX(low), MAX(close), MAX(volume) FROM quotes WHERE ticker = ? "
                     
             
             dateCommand = ""
@@ -148,37 +183,76 @@ def parseRealTimeJSON(symbol, api_key):
 
     return data
 
-def json_realtime_data(ticker):
-    ticker = str(ticker)
+# Function receives a ticker as input and returns the historical data from the database as json
+def dict_real_time_data(tickers):
+    ticker_data = []
     conn = None
     try:
+
         conn = get_db()
-        command = '''SELECT * FROM quotes WHERE date-time >= date('now', '-1 days') AND date-time < date('now);'''
-        data = {}
-        data["ticker"] = ticker
-        data["historical"] = True
-        data["quotes"] = []
-
-        cur = conn.cursor()
-        for row in cur.execute(command, (ticker, )):
-            quotes = {}
-            quotes["date-time"] = row[2]
-            quotes["open"] = row[3]
-            quotes["high"] = row[4]
-            quotes["low"] = row[5]
-            quotes["close"] = row[6]
-            quotes["volume"] = row[7]
-            data["quotes"].append(quotes)
-
-        cur.close()
-        conn.close()
-
-        jsonData = jsonify(data)
+        for ticker in tickers:
+            update_real_time_data(ticker)
+            command = "SELECT * FROM quotes WHERE ticker = ? AND date_time >= datetime('now', '-1 days') AND date_time <= datetime('now');"
             
-        return jsonData
+            data = {}
+            data["ticker"] = ticker
+            data["quotes"] = []
+
+            cur = conn.cursor()
+            for row in cur.execute(command, (ticker, )):
+                quotes = {}
+                quotes["date-time"] = row[2]
+                quotes["open"] = row[3]
+                quotes["high"] = row[4]
+                quotes["low"] = row[5]
+                quotes["close"] = row[6]
+                quotes["volume"] = row[7]
+                print(quotes)
+                data["quotes"].append(quotes)
+
+            cur.close()
+            ticker_data.append(data)
+            
+        return ticker_data
 
     except sqlite3.Error as e:
         print(e)
     finally:
         if conn is not None:
             conn.close()
+
+def update_real_time_data(ticker):
+    api_keys = ["FR2946ZQM3HAS5WL", "BC65DY8DY6N6NVI1", "L8XPCKPKDEEEPU94","3IBHJMVWO74EANY3"]
+    conn = None
+    try:
+        conn = get_db() 
+        cur = conn.cursor()
+
+        insert_cmd = '''INSERT INTO quotes (ticker, date_time, open, high, low, close, volume) VALUES (?, ?, ?, ?, ?, ?, ?)'''
+        check_exists = '''SELECT ticker, date_time FROM quotes WHERE ticker = ? AND date_time = ?'''
+
+        data = parseRealTimeJSON(ticker, api_keys[rand.randint(0, 4)])
+        time_series_data = data['Time Series (1min)']
+
+        for time_data in time_series_data:
+            entry = {}
+            for category in sorted(time_series_data[time_data]):
+                _ , column = category.split(' ')
+                entry[column] = time_series_data[time_data][category]
+
+            insert_data = (ticker, time_data, entry['open'], entry['high'], entry['low'], entry['close'], entry['volume'])
+            cur.execute(check_exists, (ticker, time_data))
+            result = cur.fetchall()
+            if len(result) == 0:
+                cur.execute(insert_cmd, insert_data)
+
+        cur.close()
+        conn.commit()
+
+    except sqlite3.Error as e:
+        print(e)
+    finally:
+        if conn is not None:
+            test = ""
+            # conn.close()
+
