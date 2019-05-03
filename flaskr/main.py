bab731ebaae67c02e6afe9b961e8395dce3d79b5
Bill Lee <bill.lee929@gmail.com>
2019-05-03 16:57:01 -0400

    dfs

--- flaskr/main.py
+++ flaskr/main.py
@@ -4,7 +4,17 @@ import sqlite3, requests, json, datetime
 from .neuralNet import nn
 import numpy as np
 
+
 bp = Blueprint('main',__name__)
+
+# def retrieve(data):
+    # global tickers=['GOOG','AMZN','NFLX','FB','AAPL','TSLA','HD','DIS','KR','ATVI']
+    # global dataset ={'GOOG':[],'AMZN':[],'NFLX':[],'FB':[],'AAPL':[],'TSLA':[],'HD':[],'DIS':[],'KR':[],'ATVI':[]}
+    # this_data = data
+    # for i in range(len(data)):
+        # for each in data[i]['quotes']:
+            # dataset[tickers[i]].append(each)
+
 
 @bp.route('/StockApp/')
 def home():
@@ -28,7 +38,7 @@ def priceFetchAPI():
     endDate = request.args.get('endDate')
 
     data = dict_historical_data(tickers, queryType, startDate, endDate)
-    print(data)
+    retrieve(data)
 
     return render_template('table.html', data=data, company_tickers=company_tickers, queryType= queryType)
 
